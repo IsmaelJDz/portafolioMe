edcce371cff3764fb077473b28577d1653262e31
IsmaelJdz <ismael_br7@hotmail.com>
2018-06-25 04:41:00 -0500

    Añadiendo mejoras de mi cv online

---
+++ README.md
@@ -0,0 +1,5 @@
+# portafolioMe
+Este es mi portafolio web 
+Puedes ver el resultado aqui
+https://ismaeljdz7.com/
+Gracias.
--- scripts/enviar.php
+++ scripts/enviar.php
@@ -2,36 +2,36 @@
 
 	//instancio un objeto de la clase PHPMailer
     $nombre=$_REQUEST["name"];
-    $telefono=$_REQUEST["subject_"];	
+    $telefono=$_REQUEST["subject_"];
     $email=$_REQUEST["email"];
     $comentarios = $_REQUEST["message"];
-    
+
 	$body="";
-	$body.="<table width='100%' style='font-weight: bold; font-size: 16px;'>";	
+	$body.="<table width='100%' style='font-weight: bold; font-size: 16px;'>";
 	$body.="<tr valign='top'>
 				<td align='right'>Nombre:</td>
 				<td align='left'>$nombre</td>
-			</tr>";	
+			</tr>";
 	$body.="<tr valign='top'>
 				<td align='right'>E-mail:</td>
 				<td align='left'>$email</td>
-			</tr>";	
+			</tr>";
 	$body.="<tr valign='top'>
 		<td align='right'>Comentarios:</td>
 		<td align='left'>$comentarios</td>
 	</tr>";
-	$body.="</table>";			
+	$body.="</table>";
 
 	require 'PHPMailerAutoload.php';
 
 	$mail = new PHPMailer;
-	
+
 	$mail->isSMTP();                                      // Set mailer to use SMTP
-	$mail->Host = 'mail.dewebnet.com';  // Specify main and backup SMTP servers
-	$mail->Port       = 26;   // set the SMTP port for the GMAIL server
+	$mail->Host = 'mail.ismaeljdz7.com';  // Specify main and backup SMTP servers
+	$mail->Port       = 25;   // set the SMTP port for the GMAIL server
 	$mail->SMTPAuth = true;                               // Enable SMTP authentication
-	$mail->Username = 'ismaeljdz@dewebnet.com';                 // SMTP username
-	$mail->Password = 'insane7_';                           // SMTP password
+	$mail->Username = '_mainaccount@ismaeljdz7.com';                 // SMTP username
+	$mail->Password = '8B92golBVWjk';                           // SMTP password
 	//$mail->SMTPSecure = 'ssl';                            // Enable encryption, 'ssl' also accepted
 	$mail->CharSet = 'UTF-8';
 	$mail->From = 'no-reply@ewebnet.com';
@@ -42,12 +42,12 @@
 	//$mail->addAddress('operaciones@gruposecom.com','operaciones');
 	$mail->addAddress('ismael_br7@hotmail.com','administracion');     // Add a recipient
 	//$mail->addAddress('erickexpress@hotmail.com','administracion');     // Add a recipient
-	
+
 	$mail->isHTML(true);                                  // Set email format to HTML
-	
+
 	$mail->Subject = "Formulario de contacto [$email]";
 	$mail->Body    = $body;
-		
+
 	if(!$mail->send()) {
 	    echo 'Message could not be sent.';
 	    echo 'Mailer Error: ' . $mail->ErrorInfo;
